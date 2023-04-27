2f2c54492587a64b98e64a2d7b2f183fb5533e7c
ogabek <quvvatullayev@gmail.com>
2023-04-27 14:36:00 +0500

    create modles

---
+++ todo/serializer.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from models import Task
+from django.contrib.auth.models import User
+
+class TaskSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    class Meta:
+        model = Task
+        fields = ('id', 'task', 'description', 'status', 'created_at', 'updated_at', 'user')
+        
+class UserSerializer(serializers.ModelSerializer):
+    tasks = serializers.PrimaryKeyRelatedField(many=True, queryset=Task.objects.all())
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'tasks')
